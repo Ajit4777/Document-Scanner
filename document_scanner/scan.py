1cdac8aebe45ecd6d760ed5eb64967eb0076087c
Ajit4777 <ajipsss4777@gmail.com>
2020-03-06 13:30:34 +0530

    Second Commit

--- document_scanner/scan.py
+++ document_scanner/scan.py
@@ -1,7 +1,3 @@
-# USAGE
-# python scan.py --image images/page.jpg
-
-# import the necessary packages
 import document_scanner.Transform.transform
 from document_scanner.Transform.transform import four_point_transform
 from skimage.filters import threshold_local
@@ -50,43 +46,3 @@ class ScanImage():
 		T = threshold_local(warped, 11, offset = 10, method = "gaussian")
 		warped = (warped > T).astype("uint8") * 255
 		return warped,imutils.resize(warped, height = 1280)
-
-
-# construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required = True,
-# 	help = "Path to the image to be scanned")
-# args = vars(ap.parse_args())
-
-# load the image and compute the ratio of the old height
-# to the new height, clone it, and resize it
-
-
-# convert the image to grayscale, blur it, and find edges
-# in the image
-
-# show the original image and the edge detected image
-
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-
-
-# loop over the contours
-
-
-# # show the contour (outline) of the piece of paper
-# print("STEP 2: Find contours of paper")
-# cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-# cv2.imshow("Outline", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# # apply the four point transform to obtain a top-down
-# # view of the original image
-
-
-# # show the original and scanned images
-# print("STEP 3: Apply perspective transform")
-# cv2.imshow("Original", imutils.resize(orig, height = 650))
-# cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-# cv2.waitKey(0)
